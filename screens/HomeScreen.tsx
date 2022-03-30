329707133e4fc66d810b8ab68364060bd0af8593
Thomas Ehmann <ehmannth@gmail.com>
2022-03-30 18:08:19 +0200

    chore: add getWorkouts()

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,28 +1,43 @@
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { useEffect, useState } from 'react';
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
-import data from '../data.json';
 import WorkoutItem from './../components/WorkoutItem';
+import { getWorkouts } from './../storage/workout';
 import { Workout } from './../types/data';
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
+	const [workouts, setWorkouts] = useState<Workout[]>([]);
+
+	useEffect(() => {
+		async function getData() {
+			const _workouts = await getWorkouts();
+			setWorkouts(_workouts);
+		}
+
+		getData();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.header}>Workout</Text>
-			<FlatList
-				data={data as Workout[]}
-				keyExtractor={item => item.slug}
-				renderItem={({ item }) => {
-					return (
-						<Pressable
-							onPress={() => {
-								navigation.navigate('WorkoutDetail', { slug: item.slug });
-							}}
-						>
-							<WorkoutItem item={item} />
-						</Pressable>
-					);
-				}}
-			/>
+			{workouts && (
+				<FlatList
+					// data={data as Workout[]}
+					data={workouts}
+					keyExtractor={item => item.slug}
+					renderItem={({ item }) => {
+						return (
+							<Pressable
+								onPress={() => {
+									navigation.navigate('WorkoutDetail', { slug: item.slug });
+								}}
+							>
+								<WorkoutItem item={item} />
+							</Pressable>
+						);
+					}}
+				/>
+			)}
 		</View>
 	);
 }
