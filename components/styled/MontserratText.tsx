79dd03da768aa9f5a6a0421738b3d04ad1792818
Thomas Ehmann <ehmannth@gmail.com>
2022-03-30 14:29:32 +0200

    chore: add style props to MontserratText component

--- components/styled/MontserratText.tsx
+++ components/styled/MontserratText.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text } from 'react-native';
 
 export default function MontserratText(props: Text['props']) {
-	return <Text style={styles.text} {...props} />;
+	return <Text style={[props.style, styles.text]} {...props} />;
 }
 
 const styles = StyleSheet.create({
