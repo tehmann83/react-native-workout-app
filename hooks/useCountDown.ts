6012c892df34dc64a20375676133b29b558e5fb5
Thomas Ehmann <ehmannth@gmail.com>
2022-03-31 17:16:14 +0200

    chore: add interval as useRef to useCountDown

--- hooks/useCountDown.ts
+++ hooks/useCountDown.ts
@@ -1,26 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const useCountDown = (idx: number, initialCount: number) => {
+    const intervalRef = useRef<number>()
     const [countDown, setCountDown] = useState(initialCount);
 
     useEffect(() => {
-		if (idx === -1) return;
+      if (idx === -1) return;
 
-		const intervalId = window.setInterval(() => {
-			setCountDown(count => {
-				console.log(count);
+      intervalRef.current = window.setInterval(() => {
+        setCountDown(count => {
+          console.log(count);
 
-				return count - 1;
-			});
-		}, 1000);
+          return count - 1;
+        });
+      }, 50);
 
-		return () => window.clearInterval(intervalId);
-	}, [idx]);
+      return cleanUp
+	  }, [idx]);
 
     useEffect(() => {
       setCountDown(initialCount)
-    }, [initialCount])
+    }, [initialCount]);
+
+    useEffect(() => {
+      if (countDown === 0) {
+          cleanUp();
+      }
+    }, [countDown])
     
+    const cleanUp = () => {
+        if (intervalRef.current) {
+            window.clearInterval(intervalRef.current)
+            intervalRef.current = undefined;
+        }
+     }
 
     return countDown;
 }
