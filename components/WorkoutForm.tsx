f714ddea553d0d513757dacb1544f8b7cd166b85
Thomas Ehmann <ehmannth@gmail.com>
2022-04-02 14:55:46 +0200

    chore: complete react hook form

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
+import PressableText from './PressableText';
 
 export type ExerciseForm = {
 	name: string;
 	duration: string;
+	reps?: string;
+	type: string;
 };
 
 type WorkoutProps = {
 	onSubmit: (form: ExerciseForm) => void;
 };
 
+const selectionItems = ['Exercise', 'Break', 'Stretch'];
+
 const WorkoutForm = ({ onSubmit }: WorkoutProps) => {
-	const { control } = useForm();
+	const { control, handleSubmit } = useForm();
+	const [isSelectionOn, setSelectionOn] = useState(false);
 
 	return (
 		<View style={styles.container}>
@@ -29,10 +35,81 @@ const WorkoutForm = ({ onSubmit }: WorkoutProps) => {
 							onChangeText={onChange}
 							value={value}
 							style={styles.input}
+							placeholder="Exercise (e.g. pushups)"
 						/>
 					)}
 				/>
+				<Controller
+					control={control}
+					rules={{
+						required: true
+					}}
+					name="duration"
+					render={({ field: { onChange, value } }) => (
+						<TextInput
+							onChangeText={onChange}
+							value={value}
+							style={styles.input}
+							placeholder="Duration (sec)"
+						/>
+					)}
+				/>
+				<Controller
+					control={control}
+					rules={{
+						required: false
+					}}
+					name="reps"
+					render={({ field: { onChange, value } }) => (
+						<TextInput
+							onChangeText={onChange}
+							value={value}
+							style={styles.input}
+							placeholder="Repetitions"
+						/>
+					)}
+				/>
+				<Controller
+					control={control}
+					rules={{
+						required: true
+					}}
+					name="type"
+					render={({ field: { onChange, value } }) => (
+						<View /* style={{ flex: 1 }} */>
+							{isSelectionOn ? (
+								<View>
+									{selectionItems.map(item => (
+										<PressableText
+											key={item}
+											style={styles.selectOption}
+											text={item}
+											onPressIn={() => {
+												setSelectionOn(false);
+												onChange(item);
+											}}
+										/>
+									))}
+								</View>
+							) : (
+								<TextInput
+									onPressIn={() => setSelectionOn(true)}
+									style={styles.input}
+									placeholder="Type"
+									value={value}
+								/>
+							)}
+						</View>
+					)}
+				/>
 			</View>
+
+			<PressableText
+				text="Submit"
+				onPress={handleSubmit(data => {
+					onSubmit(data as ExerciseForm);
+				})}
+			/>
 		</View>
 	);
 };
@@ -46,9 +123,21 @@ const styles = StyleSheet.create({
 		padding: 10
 	},
 	input: {
-		height: 40,
-		margin: 12,
 		borderWidth: 1,
-		padding: 10
+		borderRadius: 5,
+		borderColor: 'rgba(0,0,0,0.4)',
+		height: 45,
+		marginVertical: 8,
+		padding: 8
+	},
+	selectOption: {
+		height: 35,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 8,
+		marginTop: 8,
+		borderWidth: 1,
+		borderRadius: 5,
+		borderColor: '#50a9b2'
 	}
 });
