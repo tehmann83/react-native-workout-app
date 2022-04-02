2ecb1c4172025b5f233bfa63d9d1cb25b57acb60
Thomas Ehmann <ehmannth@gmail.com>
2022-04-02 20:02:58 +0200

    style: add useColorScheme

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import Navigation from './navigation';
 
 export default function App() {
 	const isLoaded = useCachedResources();
+	const colorScheme = useColorScheme();
 
 	if (isLoaded) {
 		return (
 			<>
-				<Navigation />
+				<Navigation colorScheme={colorScheme} />
 				<StatusBar style="auto" />
 			</>
 		);
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,15 +1,26 @@
 import { Entypo, FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	NavigationContainer
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { ColorSchemeName } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import PlannerScreen from '../screens/PlannerScreen';
 import WorkoutDetailScreen from '../screens/WorkoutDetailScreen';
 
-export default function Navigation() {
+export default function Navigation({
+	colorScheme
+}: {
+	colorScheme: ColorSchemeName;
+}) {
 	return (
-		<NavigationContainer>
+		<NavigationContainer
+			theme={colorScheme === 'light' ? DefaultTheme : DarkTheme}
+		>
 			<RootNavigator />
 		</NavigationContainer>
 	);
